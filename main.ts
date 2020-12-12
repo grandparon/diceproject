1630d1240e60628d85e8c9fba2ffe2a19e4ae5f0
GrandPa <75545155+grandparon@users.noreply.github.com>
2020-12-11 16:34:16 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let our_random_number = 0
+input.onGesture(Gesture.Shake, function () {
+    our_random_number = randint(1, 6)
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
