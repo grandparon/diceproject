00f7276642bb868a0dc03bdf85918e37349b2c5d
GrandPa <75545155+grandparon@users.noreply.github.com>
2020-12-11 16:36:49 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let our_random_number = 0
 input.onGesture(Gesture.Shake, function () {
     our_random_number = randint(1, 6)
-    if (true) {
+    if (our_random_number == 1) {
     	
     } else if (false) {
     	
