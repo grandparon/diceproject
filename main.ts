e096afe2cdd322f41be55f2708cbf64f37563111
GrandPa <75545155+grandparon@users.noreply.github.com>
2020-12-11 16:38:10 -0800

    added compares

--- main.ts
+++ main.ts
@@ -3,11 +3,13 @@ input.onGesture(Gesture.Shake, function () {
     our_random_number = randint(1, 6)
     if (our_random_number == 1) {
     	
-    } else if (false) {
+    } else if (our_random_number == 2) {
     	
-    } else if (false) {
+    } else if (our_random_number == 3) {
     	
-    } else if (false) {
+    } else if (our_random_number == 4) {
+    	
+    } else if (our_random_number == 5) {
     	
     } else {
     	
